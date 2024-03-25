8cfe0b4088a20228ab0861001603e0c759a2f3e8
DiabolicAugust <griffliway@gmail.com>
2024-03-25 18:28:40 +0200

    Product and wishlist functionality update

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Product, ProductDocument } from "./models/product.model";
 import { Document, Model } from "mongoose";
 import { CreateProductDto } from "./create-product.dto.js";
+import { Models, Strings } from "src/data/strings";
 
 @Injectable()
 export class ProductService {
@@ -19,12 +20,39 @@ export class ProductService {
     const product = await this.productModel.findById(id);
     if (!product)
       throw new HttpException(
-        "No product with this id",
+        Strings.objectNotFoundById(Models.Product, id),
         HttpStatus.BAD_REQUEST,
       );
     return product;
   }
+
   async getProducts() {
     return this.productModel.find();
+  }
+
+  async deleteProduct(id: string) {
+    const product = await this.productModel.findByIdAndDelete(id);
+
+    if (!product)
+      throw new HttpException(
+        Strings.objectNotFoundById(Models.Product, id),
+        HttpStatus.BAD_REQUEST,
+      );
+
+    return product;
+  }
+
+  async update(id: string, dto: CreateProductDto) {
+    const product = await this.productModel.findByIdAndUpdate(id, dto, {
+      new: true,
+    });
+
+    if (!product)
+      throw new HttpException(
+        Strings.objectNotFoundById(Models.Product, id),
+        HttpStatus.BAD_REQUEST,
+      );
+
+    return product;
   }
 }
